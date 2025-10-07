4d4df29e9b7587a4e506df2eaf141e8ce9e96fa6
Monametsi <elboncaptain@gmail.com>
2025-10-07 20:28:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App({ table }: { table: string }) {
+  console.log(table);
+  return (
+    <div className="flex items-center justify-center bg-blue-100">
+      <h1 className="text-4xl font-bold text-red-500">Tailwind's is working!</h1>
+    </div>
+  );
+}
+
+export default App;
