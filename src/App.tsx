aa6d7ee2e73b6c77f3a50cb78775899b79ea80c1
Monametsi <elboncaptain@gmail.com>
2025-10-08 15:49:48 +0200

    feat: add OverviewPage, ProductsPage and Sidebar page imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-function App({ table }: { table: string }) {
-  console.log(table);
+import { Route, Routes } from "react-router-dom";
+
+import OverviewPage from "./pages/OverviewPage.tsx";
+import ProductsPage from "./pages/ProductsPage.tsx";
+import Sidebar from "./components/common/Sidebar.tsx";
+
+
+function App() {
   return (
-    <div className="flex items-center justify-center bg-blue-100">
-      <h1 className="text-4xl font-bold text-red-500">Tailwind's is working!</h1>
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* Background */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900  via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+
+      </Routes>
+
     </div>
+
   );
 }
 
