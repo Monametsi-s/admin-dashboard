22f951113c3b5d4ad61f1592707c1aafebe77f67
Monametsi <seelemonametsi@gmail.com>
2025-11-19 19:37:38 +0200

    feat: Adjust chart layout and add sales channel chart to Overview page

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -11,13 +11,13 @@ const OverviewPage = () => {
     <div className='flex-1 overflow-auto relative z-10'>
       <Header title='Overview' />
 
-      <main className="flex-1 overflow-auto py-6 px-4 lg:px-8">
+      <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
         {/* STATS */}
         <motion.div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
-          initial={{ opacity: 0, y: 20}}
-          animate={{ opacity: 1, y: 0}}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
-          >
+        >
 
           <StatCard name="Total Sales" icon={Zap} value="$12,345" color="#6366F1" />
           <StatCard name="New Users" icon={Users} value="1,234" color="#8B5CF6" />
@@ -26,7 +26,12 @@ const OverviewPage = () => {
         </motion.div>
 
         {/* CHARTS */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 place-items-stretch gap-8">
         <SalesOverviewChart />
+        <CategoryDistributionChart />
+        <SalesChannelChart />
+        </div>
+
 
 
       </main>
