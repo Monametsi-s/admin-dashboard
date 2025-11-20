85464005db8b15fffb1651d7f90a49e3dd276a70
Monametsi <seelemonametsi@gmail.com>
2025-11-20 15:18:26 +0200

    fix: remove commented code and increase animation initial y value

--- src/pages/OverviewPage.tsx
+++ src/pages/OverviewPage.tsx
@@ -14,7 +14,7 @@ const OverviewPage = () => {
       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
         {/* STATS */}
         <motion.div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 50 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
         >
@@ -31,11 +31,7 @@ const OverviewPage = () => {
         <CategoryDistributionChart />
         <SalesChannelChart />
         </div>
-
-
-
       </main>
-
     </div>
 
 
@@ -43,41 +39,3 @@ const OverviewPage = () => {
 }
 
 export default OverviewPage;
-
-
-// import { BarChart2, ShoppingBag, Users, Zap } from "lucide-react";
-// import { motion } from "framer-motion";
-
-// import Header from "../components/common/Header";
-// import StatCard from "../components/common/StatCard";
-// import SalesOverviewChart from "../components/overview/SalesOverviewChart";
-// import CategoryDistributionChart from "../components/overview/CategoryDistributionChart";
-// import SalesChannelChart from "../components/overview/SalesChannelChart";
-
-
-//       <main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>
-//         {/* STATS */}
-//         <motion.div
-//           className='grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8'
-//           initial={{ opacity: 0, y: 20 }}
-//           animate={{ opacity: 1, y: 0 }}
-//           transition={{ duration: 1 }}
-//         >
-//           <StatCard name='Total Sales' icon={Zap} value='$12,345' color='#6366F1' />
-//           <StatCard name='New Users' icon={Users} value='1,234' color='#8B5CF6' />
-//           <StatCard name='Total Products' icon={ShoppingBag} value='567' color='#EC4899' />
-//           <StatCard name='Conversion Rate' icon={BarChart2} value='12.5%' color='#10B981' />
-//         </motion.div>
-
-//         {/* CHARTS */}
-
-//         <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-//           <SalesOverviewChart />
-//           <CategoryDistributionChart />
-//           <SalesChannelChart />
-//         </div>
-//       </main>
-//     </div>
-//   );
-// };
-// export default OverviewPage;
