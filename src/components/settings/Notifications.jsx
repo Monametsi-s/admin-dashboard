55eb81ca4a576ba8ef2d58d00d31f3fa86034398
Monametsi <seelemonametsi@gmail.com>
2025-12-11 21:51:51 +0200

    chore: Add boilerplate code for elements to put in settings page

---
+++ src/components/settings/Notifications.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import SettingSection from "./SettingSection";
+import { Bell } from "lucide-react";
+import ToggleSwitch from "./ToggleSwitch";
+const Notifications = () => {
+	return (
+		<div>Notifications</div>
+	)
+}
+
+export default Notifications
